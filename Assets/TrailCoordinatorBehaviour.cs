ab613dc68ee77413941e649ae9ddbf7f63bc9dae
Chris Zimmer <christiannorbert.zimmer@fh-duesseldorf.de>
2018-03-25 00:34:16 +0100

    PostProcess
    postProcess

---
+++ Assets/TrailCoordinatorBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailCoordinatorBehaviour : MonoBehaviour {
+
+	float startScale;
+	public float scaleIntensity = 1000;
+	// Use this for initialization
+	void Start () {
+		startScale = (float)GetComponent<TrailRenderer>().widthMultiplier;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<TrailRenderer>().widthMultiplier = startScale + Camera.main.orthographicSize/scaleIntensity;
+	}
+}
