aabcf165f33d2c4690e9b72d481475e06c15fc3c
Chris Zimmer <christiannorbert.zimmer@fh-duesseldorf.de>
2018-03-25 23:05:18 +0200

    PlanetMarkerContainer
    PlanetMarkerContainer

--- Assets/TimelineCoordinatorBehaviour.cs
+++ Assets/TimelineCoordinatorBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TimelineCoordinatorBehaviour : MonoBehaviour {
 
 	public RectTransform timelineContainer;
+	public RectTransform timeStepContainer;
+	public RectTransform planetMarkerContainer;
+
 	public GameObject timeStepPrefab;
 	public GameObject currentPositionMarker;
 	public GameObject planetPositionMarker;
@@ -17,24 +20,30 @@ public class TimelineCoordinatorBehaviour : MonoBehaviour {
 		
 	}
 
+	int timeStepChoice = 0;
 	public void CreateTimestepsMinutes(){
 		CreateTimesteps (0);
+		timeStepChoice = 0;
 	}
 	public void CreateTimestepsHours(){
 		CreateTimesteps (1);
+		timeStepChoice = 1;
 	}
 	public void CreateTimestepsDays(){
 		CreateTimesteps (2);
+		timeStepChoice = 2;
 	}
 	public void CreateTimestepsMonths(){
 		CreateTimesteps (3);
+		timeStepChoice = 3;
 	}
 	public void CreateTimestepsYears(){
 		CreateTimesteps (4);
+		timeStepChoice = 4;
 	}
 		
 	public void CreateTimesteps(int type){
-		foreach (RectTransform t in timelineContainer){
+		foreach (RectTransform t in timeStepContainer){
 			Destroy (t.gameObject);
 		}
 
@@ -48,40 +57,46 @@ public class TimelineCoordinatorBehaviour : MonoBehaviour {
 
 		if (type == 0) {
 			timeStep = (int)(containerSize / minutes);
-			Debug.Log("Minutes needed:" + minutes);
+			//Debug.Log("Minutes needed:" + minutes);
 		} else if (type == 1) {
 			timeStep = (int)(containerSize / hours);
-			Debug.Log("Hours needed:" + hours);
+			//Debug.Log("Hours needed:" + hours);
 		} else if (type == 2) {
 			timeStep = (int)(containerSize / days);
-			Debug.Log("Days needed:" + days);
+			//Debug.Log("Days needed:" + days);
 		} else if (type == 3) {
 			timeStep = (int)(containerSize / months);
-			Debug.Log("Months needed:" + months);
+			//Debug.Log("Months needed:" + months);
 		} else if (type == 4) {
 			timeStep = (int)(containerSize / years);
-			Debug.Log("Years needed:" + years);
+			//Debug.Log("Years needed:" + years);
 		}
 
 
 		// CREATE TIMESTEPS IN TIMELINE
 		if ((int)timeStep > 0) {
 			for (int i = timeStep; i < containerSize; i += timeStep) {
-				GameObject timeStep = Instantiate (timeStepPrefab, timelineContainer, false);
+				GameObject timeStep = Instantiate (timeStepPrefab, timeStepContainer, false);
 				timeStep.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (i, 0);
 			}
 		}
+	}
 
+	void CreatePlanetMarkersInTimeline(){
 		// CREATE PLANET MARKERS IN TIMELINE
+		foreach (RectTransform t in planetMarkerContainer){
+			Destroy (t.gameObject);
+		}
+		float containerSize = timelineContainer.rect.width;
 		if (movementBehaviour.destinationPlanet.transform.position.x > movementBehaviour.startPlanet.position.x) {
 
 			foreach (GameObject planet in coordinateSystemCreator.planets) {
 				float distance = Mathf.Abs (planet.transform.position.x - movementBehaviour.startPlanet.position.x);
 
 				if (distance < movementBehaviour.distanceComplete && (planet.transform.position.x > movementBehaviour.startPlanet.position.x)) {
-					Debug.Log (distance+ "dis");
-					Debug.Log (movementBehaviour.distanceComplete+"disComp");
-					GameObject planetMarker = Instantiate (planetPositionMarker, timelineContainer, false);
+					//Debug.Log (distance+ "dis");
+					//Debug.Log (movementBehaviour.distanceComplete+"disComp");
+					GameObject planetMarker = Instantiate (planetPositionMarker, planetMarkerContainer, false);
 					planetMarker.GetComponent<RectTransform> ().anchoredPosition = new Vector2 ((float)(distance/movementBehaviour.distanceComplete) * containerSize, 0);
 				}
 			}
@@ -91,15 +106,16 @@ public class TimelineCoordinatorBehaviour : MonoBehaviour {
 				float distance = Mathf.Abs (planet.transform.position.x - movementBehaviour.startPlanet.position.x);
 
 				if (distance < movementBehaviour.distanceComplete && (planet.transform.position.x < movementBehaviour.startPlanet.position.x)) {
-					Debug.Log (distance+ "dis");
-					Debug.Log (movementBehaviour.distanceComplete+"disComp");
-					GameObject planetMarker = Instantiate (planetPositionMarker, timelineContainer, false);
+					//Debug.Log (distance+ "dis");
+					//Debug.Log (movementBehaviour.distanceComplete+"disComp");
+					GameObject planetMarker = Instantiate (planetPositionMarker, planetMarkerContainer, false);
 					planetMarker.GetComponent<RectTransform> ().anchoredPosition = new Vector2 ((float)(distance/movementBehaviour.distanceComplete) * containerSize, 0);
 				}
 			}
 		}
-
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -107,6 +123,7 @@ public class TimelineCoordinatorBehaviour : MonoBehaviour {
 
 		if (movementBehaviour.distanceToStart != 0) {
 			currentPositionMarker.GetComponent<RectTransform>().anchoredPosition = new Vector2 (timelineContainer.rect.width * ((float)movementBehaviour.distanceToStart/(float)movementBehaviour.distanceComplete), 0);
+			CreatePlanetMarkersInTimeline();
 		}
 	}
 }
