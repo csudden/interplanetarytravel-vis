d79c31216049d14887bae1c49eb5f9e1da11d98f
Chris Zimmer <christiannorbert.zimmer@fh-duesseldorf.de>
2018-03-25 17:21:27 +0200

    BuildTimesteps
    BuildTimestepsTimeline

---
+++ Assets/TimelineCoordinatorBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineCoordinatorBehaviour : MonoBehaviour {
+
+	public RectTransform timelineContainer;
+	public GameObject timeStepPrefab;
+	public int timeStep = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void CreateTimesteps(){
+		foreach (RectTransform t in timelineContainer){
+			Destroy (t.gameObject);
+		}
+
+		float containerSize = timelineContainer.rect.width;
+
+		for (int i = timeStep; i < containerSize; i += timeStep) {
+			GameObject timeStep = Instantiate (timeStepPrefab, timelineContainer, false);
+			timeStep.GetComponent<RectTransform> ().anchoredPosition = new Vector2(i,0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
